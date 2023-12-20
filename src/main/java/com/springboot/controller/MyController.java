f09653d2748b6f894330d676454b215788bb4ddf
Istkaar <istkaar.ali2022@gmail.com>
2023-12-20 15:57:27 +0530

    mongo dbproject

---
+++ src/main/java/com/springboot/controller/MyController.java
@@ -0,0 +1,38 @@
+package com.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.entities.Student;
+import com.springboot.rep.StudentRepository;
+
+@RestController
+@RequestMapping("/student")
+public class MyController {
+	
+	@Autowired
+	private StudentRepository studentRepository;
+	@PostMapping("/")
+	public ResponseEntity<?> addStudent(@RequestBody Student student){
+		Student save = this.studentRepository.save(student);
+		return ResponseEntity.ok(save);
+	}
+	
+	
+	
+	@GetMapping("/")
+	public ResponseEntity<?> getStudent(){
+	return ResponseEntity.ok(this.studentRepository.findAll());
+	}
+	
+	
+	
+	
+	
+	
+}
